2ec7ca8780709fb7846110a91811da860f64b8b7
unknown <jbaycroft@CAP099.Capaccio.local>
2013-03-27 12:19:16 -0400

    fix procedure steps and header for Table A

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -27,7 +27,6 @@ class FormsController < ApplicationController
     3.times do
       authemp = @form.authemps.build
     end
-    @procedures = @form.procedures.order('step_num ASC')
 
 		respond_to do |format|
       		format.html # show.html.erb
