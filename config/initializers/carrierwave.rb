1ddba12d4160e245c75a010fed2ff1c446d6fe84
John Baycroft <jbaycroft@capaccio.com>
2013-11-18 15:30:15 -0500

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAJSEOK6PIDD5OG7LQ',
     :aws_secret_access_key  => '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'
   }
-  config.fog_directory  = 'cap_loto'
+  config.fog_directory  = 'cap_demo'
 end
